9190724fe88dd80d2471111ee21a32fa0651c392
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2022-03-10 10:30:11 +0000

    Finished writing script

--- Save The Savannah/Assets/scripts/HelloWorld.cs
+++ Save The Savannah/Assets/scripts/HelloWorld.cs
@@ -1,3 +1,8 @@
+//Hello World
+//Author: Robert Bulmer
+//Date 10/03/22
+
+//Libraries used
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +12,7 @@ public class HelloWorld : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
