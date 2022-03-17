93721b1ac5b7a7f20e0cfd563e4a719fba36a170
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2022-03-17 10:43:32 +0000

    Made a movement script

---
+++ Save The Savannah/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 2;
+    private Rigidbody2D physicsBody = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        physicsBody = GetComponent<Rigidbody2D>();
+
+        //physicsBody.velocity = new Vector2(2, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float axisVal = Input.GetAxis("Horizontal");
+
+        float axisVal2 = Input.GetAxis("Vertical");
+        physicsBody.velocity = new Vector2(axisVal * speed, axisVal2 * speed);
+
+        // (x axis value, y axis value)
+    }
+}
