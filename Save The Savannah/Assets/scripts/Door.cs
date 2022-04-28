733a1b89ad14c454eacf28415c6f506c95139dd4
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2022-04-28 10:53:25 +0100

    added door what changes the level

---
+++ Save The Savannah/Assets/scripts/Door.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // The thing we ran into had the player tag 
+
+            SceneManager.LoadScene("Level 1");
+        }
+    }
+
+}
