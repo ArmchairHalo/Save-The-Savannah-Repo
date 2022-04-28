151ae47c9483127e2017c5ee699f3f0343fb450b
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2022-04-28 10:33:14 +0100

    added script to move character through buttons (not functioning)

---
+++ Save The Savannah/Assets/scripts/PlayerMovementButtons.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementButtons : MonoBehaviour
+{
+    public void MoveLeft()
+    {
+
+    }
+
+    public void MoveRight()
+    {
+
+    }
+}
