27ed4bee73ea2e0bfb8fb9b3b388b93c73ae3bd6
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2022-06-14 22:10:10 +0100

    made enemy patrol script and replaced test sprite with main character sprite

---
+++ Save The Savannah/Assets/scripts/EnemyPatrol.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float walkSpeed;
+    [HideInInspector]
+    public bool mustPatrol;
+    private bool mustTurn;
+
+    public Rigidbody2D rb;
+    public Transform groundCheckPos;
+    public LayerMask groundLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mustPatrol = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mustPatrol)
+        {
+            patrol();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (mustPatrol)
+        {
+            mustTurn = !Physics2D.OverlapCircle(groundCheckPos.position, 0.1f, groundLayer);
+        }
+    }
+
+    void patrol()
+    {
+        if (mustTurn)
+        {
+            Flip();
+        }
+        rb.velocity = new Vector2(walkSpeed * Time.fixedDeltaTime, rb.velocity.y);
+    }
+    void Flip()
+    {
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
+        walkSpeed *= -1;
+        mustPatrol = true;
+    }
+
+
+}
