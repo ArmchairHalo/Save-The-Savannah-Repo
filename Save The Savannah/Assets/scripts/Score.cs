a4255f520b00bc2dfab9fd33c8acd9ff1d70b531
ArmchairHalo <79139275+ArmchairHalo@users.noreply.github.com>
2022-06-17 21:14:00 +0100

    made it so that the score script displays the current score when it goes to the next level & prepared the next level for later

--- Save The Savannah/Assets/scripts/Score.cs
+++ Save The Savannah/Assets/scripts/Score.cs
@@ -7,9 +7,23 @@ public class Score : MonoBehaviour
 {
     //public variables
     public Text scoreDisplay;
-    
+    public bool shouldReset = false;
+
     //private variables
-    private int scoreValue = 0;
+    private static int scoreValue = 0;
+
+    //Called by unity the frame after this object is created
+    void start()
+    {
+        //check if we should be reseting the score this scene
+        if(shouldReset = true)
+        {
+            //reset the score value back to 0
+            scoreValue = 0;
+        }
+        //Update the display of the score based on the numerical value
+        scoreDisplay.text = scoreValue.ToString();
+    }
 
     //function to add to the player's score 
     public void AddScore(int toAdd)
